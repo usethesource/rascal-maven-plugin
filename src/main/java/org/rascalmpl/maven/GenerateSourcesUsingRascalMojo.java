d342e59405705800cbdec7246bbd1c1a264504a8
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-06-10 11:15:44 +0200

    changed generate-sources mojo to start a nested JVM process in order to separate classpath of the plugin from the classpath of the project that is using rascal to generate sources

--- src/main/java/org/rascalmpl/maven/GenerateSourcesUsingRascalMojo.java
+++ src/main/java/org/rascalmpl/maven/GenerateSourcesUsingRascalMojo.java
@@ -16,6 +16,7 @@ import java.io.StringReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -54,105 +55,44 @@ import io.usethesource.vallang.io.StandardTextReader;
 @Mojo(name="generate-sources", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class GenerateSourcesUsingRascalMojo extends AbstractMojo
 {
-    private static final String UNEXPECTED_ERROR = "unexpected error during Rascal run";
-
     @Parameter(defaultValue="${project}", readonly=true, required=true)
     private MavenProject project;
-
-    @Parameter(defaultValue="${project.build.directory}/generated", required=true)
-    private String generated;
 
     @Parameter(property = "mainModule", required=true)
     private String mainModule;
 
-    @Parameter(property = "mainFunction", required=true)
-    private String mainFunction;
-
-    @Parameter(property = "libs", required=false)
-    private List<String> libs;
-
-    @Parameter(property = "srcs", required=true)
-    private List<String> srcs;
-
-    private final  MojoRascalMonitor monitor = new MojoRascalMonitor(getLog(), false);
-
-    private Evaluator makeEvaluator(PathConfig pcfg) throws URISyntaxException, FactTypeUseException, IOException {
-
-		ISourceLocation[] searchPath = new ISourceLocation[libs.size() + srcs.size()];
-		int spIndex = 0;
-		for (String lib : libs) {
-			searchPath[spIndex++] = MojoUtils.location(lib);
-		}
-        for (String src: srcs) {
-			searchPath[spIndex++] = MojoUtils.location(src);
-		}
-
-        URIResolverRegistry.getInstance().registerLogical(new ILogicalSourceLocationResolver() {
-            ISourceLocation root = URIUtil.createFileLocation(project.getBasedir().getAbsolutePath());
-
-            @Override
-            public String scheme() {
-                return "project";
-            }
-
-            @Override
-            public ISourceLocation resolve(ISourceLocation input) throws IOException {
-                return URIUtil.getChildLocation(root, input.getPath());
-            }
-
-            @Override
-            public String authority() {
-                return project.getName();
-            }
-        });
-
-		return MojoUtils.makeEvaluator(
-			getLog(),
-			monitor,
-			System.err,
-			System.out,
-			searchPath,
-			mainModule
-		);
-    }
-
     public void execute() throws MojoExecutionException {
-        Evaluator eval = null;
+	    String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+
+        List<String> command = new LinkedList<String>();
+        command.add(javaBin);
+        command.add("-cp");
+        command.add(collectClasspath());
+        command.add("org.rascalmpl.shell.RascalShell");
+        command.add(mainModule);
 
         try {
-            ISourceLocation binLoc = MojoUtils.location(generated);
-            List<ISourceLocation> srcLocs = MojoUtils.locations(srcs);
-            List<ISourceLocation> libLocs = MojoUtils.locations(libs);
-
-            MojoUtils.collectDependentArtifactLibraries(project, libLocs);
-
-            for (ISourceLocation lib : libLocs) {
-                getLog().info("\tregistered library location: " + lib);
-            }
-
-            getLog().info("paths have been configured");
-
-            PathConfig pcfg = new PathConfig(srcLocs, libLocs, binLoc);
-
-            eval = makeEvaluator(pcfg);
-
-            eval.call(monitor, mainFunction, pcfg.asConstructor());
-
-            getLog().info(mainFunction + " is done.");
-
-            return;
-        } catch (URISyntaxException | IOException e) {
-            throw new MojoExecutionException(UNEXPECTED_ERROR, e);
-        } catch (Throw e) {
-            getLog().error(e.getLocation() + ": " + e.getMessage());
-            getLog().error(e.getTrace().toString());
-            throw new MojoExecutionException(UNEXPECTED_ERROR, e);
-        } catch (StaticError e) {
-            if (eval != null) {
-                getLog().error(e.getLocation() + ": " + e.getMessage());
-                getLog().error(eval.getStackTrace().toString());
-            }
-            throw e;
+            ProcessBuilder builder = new ProcessBuilder(command);
+            Process process = builder.inheritIO().start();
+            process.waitFor();
+        } catch (IOException e) {
+            getLog().error(e);
+        } catch (InterruptedException e) {
+            getLog().warn(e);
         }
+        finally {}
+    }
+
+    private String collectClasspath() {
+	    StringBuilder builder = new StringBuilder();
+
+        for (Object o : project.getArtifacts()) {
+            Artifact a = (Artifact) o;
+            File file = a.getFile().getAbsoluteFile();
+            builder.append(":" + file.getAbsolutePath());
+        }
+
+        return builder.toString().substring(1);
     }
 }

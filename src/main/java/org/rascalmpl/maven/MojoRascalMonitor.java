78d71b97882602099e05979ce354ebd0aa9a09ef
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2021-09-15 11:55:53 +0200

    added missing @Override annotations first

--- src/main/java/org/rascalmpl/maven/MojoRascalMonitor.java
+++ src/main/java/org/rascalmpl/maven/MojoRascalMonitor.java
@@ -14,17 +14,21 @@ public class MojoRascalMonitor implements IRascalMonitor {
 		this.chatty = chatty;
 	}
 
+	@Override
 	public void startJob(String name) {
 	}
 
+	@Override
 	public void startJob(String name, int totalWork) {
 		startJob(name);
 	}
 
+	@Override
 	public void startJob(String name, int workShare, int totalWork) {
 		startJob(name);
 	}
 
+	@Override
 	public void event(String name) {
 	    if (chatty) {
 			synchronized (log) {
@@ -33,27 +37,33 @@ public class MojoRascalMonitor implements IRascalMonitor {
 	    }
 	}
 
+	@Override
 	public void event(String name, int inc) {
 		event(name);
 
 	}
 
+	@Override
 	public void event(int inc) {
 
 	}
 
+	@Override
 	public int endJob(boolean succeeded) {
 		return 0;
 	}
 
+	@Override
 	public boolean isCanceled() {
 		return false;
 	}
 
+	@Override
 	public void todo(int work) {
 
 	}
 
+	@Override
 	public void warning(String message, ISourceLocation src) {
 		synchronized (log) {
 			log.warn(src.toString() + ": " + message);

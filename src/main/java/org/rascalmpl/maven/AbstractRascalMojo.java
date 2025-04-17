2f22336aba894b876131deb816fc7857b2ce2f4a
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-04-17 13:06:36 +0200

    reintroduced the merging of the screenshot feature into the classpath and added some debugging features

--- src/main/java/org/rascalmpl/maven/AbstractRascalMojo.java
+++ src/main/java/org/rascalmpl/maven/AbstractRascalMojo.java
@@ -83,14 +83,14 @@ public abstract class AbstractRascalMojo extends AbstractMojo
 	protected boolean verbose;
 
 	@Parameter(defaultValue = "${session}", required = true, readonly = true)
-  	private MavenSession session;
+	protected MavenSession session;
 
 	@Parameter(defaultValue = "0.41.0-RC32", required = false, readonly = false)
 	protected String bootstrapRascalVersion;
 
 	@SuppressWarnings("deprecation") // Can't get @Parameter to work for the pluginManager.
 	@Component
-	private BuildPluginManager pluginManager;
+	protected BuildPluginManager pluginManager;
 
 	@Parameter(defaultValue="")
 	protected String mainModule;
@@ -132,20 +132,6 @@ public abstract class AbstractRascalMojo extends AbstractMojo
 		}
 
 		return cachedRascalRuntime;
-	}
-
-	protected String getScreenShotJar() {
-		for (Object o : project.getArtifacts()) {
-			Artifact a = (Artifact) o;
-
-			if (a.getGroupId().equals("org.rascalmpl") && a.getArtifactId().equals("rascal-tutor-screenshot")) {
-				File file = a.getFile().getAbsoluteFile();
-
-				return file.toString();
-			}
-		}
-
-		return "";
 	}
 
 	/**
@@ -362,7 +348,11 @@ public abstract class AbstractRascalMojo extends AbstractMojo
 			}
 		}
 
-		getLog().debug("Starting process: " + command.get(0) + command.stream().collect(Collectors.joining("\n\t")));
+		getLog().debug("Starting process:\n\t" + command.get(0) +
+			command.stream()
+			.map(s -> s.replaceAll("\n", "\\\\n"))
+			.map(s -> "'" + s + "'")
+			.collect(Collectors.joining(" ")));
 
 		assert command.stream().map(Objects::nonNull).allMatch(b -> b) : "command had a null parameter";
 

e90ff13bf8a56bc8a7a76f052f4f10bcf5effc58
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2025-04-22 14:17:06 +0200

    bumped minimal rascal boot version to 0.41.0-RC38

--- src/main/java/org/rascalmpl/maven/CompileRascalMojo.java
+++ src/main/java/org/rascalmpl/maven/CompileRascalMojo.java
@@ -29,7 +29,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;

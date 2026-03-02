39da1c9532b849cda4bf4ee5d1e3d6e865ff4fb9
Davy Landman <davy.landman@swat.engineering>
2026-03-02 15:22:25 +0100

    Using latest rascal as default

--- src/main/java/org/rascalmpl/maven/AbstractRascalMojo.java
+++ src/main/java/org/rascalmpl/maven/AbstractRascalMojo.java
@@ -88,7 +88,7 @@ public abstract class AbstractRascalMojo extends AbstractMojo
 	@Parameter(defaultValue = "${session}", required = true, readonly = true)
 	protected MavenSession session;
 
-	@Parameter(defaultValue = "0.41.3-RC1", required = false, readonly = false)
+	@Parameter(defaultValue = "0.42.0", required = false, readonly = false)
 	protected String bootstrapRascalVersion;
 
 	@SuppressWarnings("deprecation") // Can't get @Parameter to work for the pluginManager.
@@ -278,7 +278,7 @@ public abstract class AbstractRascalMojo extends AbstractMojo
 	}
 
 	protected final ArtifactVersion getReferenceRascalVersion() {
-		return new DefaultArtifactVersion("0.41.3-RC1");
+		return new DefaultArtifactVersion("0.42.0");
 	}
 
 	protected Path installBootstrapRascalVersion(MavenProject project, MavenSession session) throws MojoExecutionException {
